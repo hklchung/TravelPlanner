f8b27653f574f97334966b2523859c027aa4beea
Leslie Chung <hklchung@gmail.com>
2020-03-16 22:20:10 +1100

    Updated settings for hosting in AWS

--- embark_web/embark/embark/settings.py
+++ embark_web/embark/embark/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't=8z4kzo*@u(qo#%y$+v()@k3cpb!4ox@n2#x=@6w=ytrysjca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
