28f9e851a7b0b6d892f8f3a124307fa1c82c3ba0
Edward Ho <mfho92@gmail.com>
2020-04-19 22:19:08 +1000

    app framework modified

--- embark_web/venv/bin/django-admin 2.py
+++
@@ -1,5 +0,0 @@
-#!/Users/edward/Documents/Programming/Official_Embark/TravelPlanner/embark_web/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
